0f73f64350c523b0cd42af602d0006a505830121
leo <romain@boudet.me>
2024-11-09 14:54:00 +0100

    add Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // antialiased => Tailwind CSS => Utilities for controlling the font smoothing of an element.
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
